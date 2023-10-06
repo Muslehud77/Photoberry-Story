5fd42e50594fca835d35c267e492a0eb347da949
Muslehud77 <fardinmohit@gmail.com>
2023-10-07 01:04:07 +0800

    first commit

---
+++ src/Root/Root.jsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+
+
+const Root = () => {
+ 
+    return (
+        <div>
+
+            <h1>Hello</h1>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Root;
