a83ab7d65f7b898aec0868594dd4503ca8e3e88b
Muslehud77 <fardinmohit@gmail.com>
2023-10-10 01:41:28 +0800

    finishing-touches

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -79,104 +79,106 @@ const socialLogin = (media) => {
 
 
   return (
-    <section className="w-full relative bg-black bg-opacity-60 px-8 py-20 grid grid-cols-1 md:grid-cols-2 items-center gap-8 ">
-      <Helmet>
-        <title>Photoberry | Login</title>
-      </Helmet>
-      <div>
-        <div
-          data-aos="zoom-in-down"
-          className="max-w-md relative z-10 border py-10 px-10 md:px-12 mx-auto space-y-8 bg-white rounded-md"
-        >
-          <h2 className="font-semibold text-3xl">Login your account</h2>
-          <hr />
-          <form onSubmit={handleSubmit} className="space-y-4">
-            <p className="font-semibold">Your Email</p>
-            <input
-              required
-              type="email"
-              name="email"
-              placeholder="Enter your email"
-              className="input input-bordered w-full bg-gray-100"
-            />
-            <p className="font-semibold">Password</p>
-            <div className="relative">
+    <div className="relative bg-black bg-opacity-60 ">
+      <section className="w-full container  mx-auto px-8 py-20 grid grid-cols-1 md:grid-cols-2 items-center gap-8 ">
+        <Helmet>
+          <title>Photoberry | Login</title>
+        </Helmet>
+        <div className="">
+          <div
+            data-aos="zoom-in-down"
+            className="max-w-md relative z-10 border py-10 px-10 md:px-12 mx-auto space-y-8 bg-white rounded-md"
+          >
+            <h2 className="font-semibold text-3xl">Login your account</h2>
+            <hr />
+            <form onSubmit={handleSubmit} className="space-y-4">
+              <p className="font-semibold">Your Email</p>
               <input
-                id="password"
                 required
-                type={`${show ? "text" : "password"}`}
-                name="password"
-                placeholder="Password "
-                className="input input-bordered w-full bg-gray-100 text-black"
+                type="email"
+                name="email"
+                placeholder="Enter your email"
+                className="input input-bordered w-full bg-gray-100"
               />
-              <div
-                className="hover:cursor-pointer absolute right-3 bottom-3 text-black text-xl"
-                onClick={() => {
-                  setShow(!show);
-                }}
-              >
-                {!show ? <AiFillEyeInvisible /> : <AiFillEye />}
+              <p className="font-semibold">Password</p>
+              <div className="relative">
+                <input
+                  id="password"
+                  required
+                  type={`${show ? "text" : "password"}`}
+                  name="password"
+                  placeholder="Password "
+                  className="input input-bordered w-full bg-gray-100 text-black"
+                />
+                <div
+                  className="hover:cursor-pointer absolute right-3 bottom-3 text-black text-xl"
+                  onClick={() => {
+                    setShow(!show);
+                  }}
+                >
+                  {!show ? <AiFillEyeInvisible /> : <AiFillEye />}
+                </div>
               </div>
-            </div>
-            <div className="space-y-2">
-              {err && (
-                <p className="capitalize text-red-500 font-serif  rounded-xl">
-                  {err}
-                </p>
-              )}
-              <input
-                type="submit"
-                value="login"
-                className="btn btn-neutral w-full"
-              />
-            </div>
-          </form>
-          <div>
-            <span>
-              Don't Have An Account ?{" "}
-              <Link
-                state={state}
-                className="hover:font-semibold text-blue-700"
-                to="/register"
-              >
-                Register
-              </Link>
-            </span>
-            <div className="mt-3">
-              <div className="flex justify-center gap-5 items-center mb-2">
-                <div className="w-16 h-[1px] bg-black"></div>
-                <h2 className="text-xl font-bold ">or</h2>
-                <div className="w-16 h-[1px] bg-black"></div>
-              </div>
-
               <div className="space-y-2">
-                <button
-                  onClick={() => socialLogin(googleSignIn)}
-                  className="btn w-full btn-outline"
+                {err && (
+                  <p className="capitalize text-red-500 font-serif  rounded-xl">
+                    {err}
+                  </p>
+                )}
+                <input
+                  type="submit"
+                  value="login"
+                  className="btn btn-neutral w-full"
+                />
+              </div>
+            </form>
+            <div>
+              <span>
+                Don't Have An Account ?{" "}
+                <Link
+                  state={state}
+                  className="hover:font-semibold text-blue-700"
+                  to="/register"
                 >
-                  <FcGoogle className="text-2xl" /> continue with Google
-                </button>
-                <button
-                  onClick={() => socialLogin(facebookSignIn)}
-                  className="btn w-full btn-outline"
-                >
-                  {" "}
-                  <BsFacebook className="text-2xl" /> continue with Facebook
-                </button>
+                  Register
+                </Link>
+              </span>
+              <div className="mt-3">
+                <div className="flex justify-center gap-5 items-center mb-2">
+                  <div className="w-16 h-[1px] bg-black"></div>
+                  <h2 className="text-xl font-bold ">or</h2>
+                  <div className="w-16 h-[1px] bg-black"></div>
+                </div>
+
+                <div className="space-y-2">
+                  <button
+                    onClick={() => socialLogin(googleSignIn)}
+                    className="btn w-full btn-outline"
+                  >
+                    <FcGoogle className="text-2xl" /> continue with Google
+                  </button>
+                  <button
+                    onClick={() => socialLogin(facebookSignIn)}
+                    className="btn w-full btn-outline"
+                  >
+                    {" "}
+                    <BsFacebook className="text-2xl" /> continue with Facebook
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div data-aos="zoom-in-up" className="relative z-10">
-        <ShuffleGrid />
-      </div>
-      <img
-        className="absolute top-0 w-full h-full object-cover opacity-50"
-        src={bg}
-        alt=""
-      />
-    </section>
+        <div data-aos="zoom-in-up" className="z-10">
+          <ShuffleGrid />
+        </div>
+        <img
+          className="absolute left-0 top-0 w-screen h-full object-cover opacity-50"
+          src={bg}
+          alt=""
+        />
+      </section>
+    </div>
   );
 };
 
